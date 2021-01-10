097a51fe5d6de09c578e1e2914f4738573bd163e
taaako <ta12ko.sa@gmail.com>
2021-01-11 04:22:02 +0900

    059_Bとその別解の追加

---
+++ ABC/059_Bi.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str_a, str_b;
+    cin >> str_a >> str_b;
+
+    if(str_a.size() != str_b.size())
+    {
+        if (str_a.size() > str_b.size())
+            cout << "GREATER" << endl;
+        else
+            cout << "LESS" << endl;
+        return 0;
+    }
+    for (int i= 0, l = str_a.size(); i < l; ++i)
+        if (str_a.at(i) > str_b.at(i))
+        {
+            cout << "GREATER" << endl;
+            return 0;
+        }
+        else if (str_a.at(i) < str_b.at(i))
+        {
+            cout << "LESS" << endl;
+            return 0;
+        }
+    cout << "EQUAL" << endl;
+}
