7bf744f83b10c040d705124bf093413f2dadd694
taaako <ta12ko.sa@gmail.com>
2021-01-13 12:23:20 +0900

    ex11の追加

---
+++ APG4b/ex11.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N, A;
+  cin >> N >> A;
+
+  // ここにプログラムを追記
+  for (int i = 0; i < N; i++) {
+	string op;
+    int B;
+    cin >> op >> B;
+    
+    if (op == "+") {
+      A += B;
+    }else if (op == "-") {
+      A -= B;
+    }else if (op == "*") {
+      A *= B;
+    }else{
+      if (B == 0) {
+        cout << "error" << endl;
+      	break;
+      }
+      A /= B;
+    }
+    cout << i+1 << ":" << A << endl;
+  }
+}
