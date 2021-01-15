3f0854b22d7a5970ceef1cc68707a627f88fa505
taaako <ta12ko.sa@gmail.com>
2021-01-16 02:41:26 +0900

    ex6_練習問題の追加

---
+++ ABC/083_A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int a,b,c,d;
+  cin >> a >> b >> c >> d;
+  
+  int l = 0;
+  l = a + b;
+  int r = 0;
+  r = c + d;
+  
+  if (l > r)
+    cout << "Left" << endl;
+  else if (l == r)
+    cout << "Balanced" << endl;
+  else
+    cout << "Right" << endl;
+}
