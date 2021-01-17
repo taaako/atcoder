dc3ee5a5c4ba9435cf0ce5ed9e809fc1972d4126
taaako <ta12ko.sa@gmail.com>
2021-01-18 01:25:23 +0900

    ex12_練習問題の追加

---
+++ ABC/080_B.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+  int n;
+  cin >> n;
+  
+  int n1 = n / 10;
+  int n2 = n1 / 10
+  if (n%f == 0){
+   cout << "Yes" << endl;
+  }else{
+    cout << "No" << endl;
+  }
+}
