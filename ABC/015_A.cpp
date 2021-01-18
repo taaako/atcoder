e87aaff364bf403771e90ceb65856f4dec9fe038
taaako <ta12ko.sa@gmail.com>
2021-01-18 15:30:49 +0900

    ex12_練習問題の追加

---
+++ ABC/015_A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  string a, b;
+  cin >> a >> b;
+  
+  if (a.size() > b.size())
+    cout << a << endl;
+  else
+    cout << b << endl;
+}
