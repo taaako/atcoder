9e33fe75e224a4fb23420b8143db8a8877142163
taaako <ta12ko.sa@gmail.com>
2021-01-18 15:22:06 +0900

    ex12_練習問題の追加

---
+++ ABC/038_A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  string s;
+  cin >> s;
+  
+  if (s.at(s.size()-1) == 'T')
+    cout << "YES" << endl;
+  else
+    cout << "NO" << endl;
+}
