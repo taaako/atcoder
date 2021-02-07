9deaf3d69829bcc0b9cd9f165365f3f220fe7762
taaako <ta12ko.sa@gmail.com>
2021-02-08 02:25:38 +0900

    ACS_100の追加

---
+++ ABC/042_A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a[3];
+    cin >> a[0] >> a[1] >> a[2];
+
+    sort (a, a+3);
+    if (a[0]==5 && a[1]==5 && a[2]==7)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    return 0;
+}
