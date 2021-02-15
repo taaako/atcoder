4bcc0051c7d3064d20c7e5d85200d28d3ed32b53
taaako <ta12ko.sa@gmail.com>
2021-02-15 17:24:58 +0900

    ACS_100の追加

---
+++ ABC/059_A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s1, s2, s3;
+    cin >> s1 >> s2 >> s3;
+
+    s1[0] = toupper(s1[0]);
+    s2[0] = toupper(s2[0]);
+    s3[0] = toupper(s3[0]);
+    cout << s1[0] << s2[0] << s3[0] << endl;
+}
