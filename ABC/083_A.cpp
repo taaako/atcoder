f4e3bbb5c403d41bc0ef25e2c1af9867d78d79e4
taaako <ta12ko.sa@gmail.com>
2021-03-02 12:40:10 +0900

    ex6_練習問題の追加

--- ABC/083_A.cpp
+++ ABC/083_A.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main(){
   int a,b,c,d;
   cin >> a >> b >> c >> d;
-  
+
   int l = 0;
   l = a + b;
   int r = 0;
   r = c + d;
-  
+
   if (l > r)
     cout << "Left" << endl;
   else if (l == r)
