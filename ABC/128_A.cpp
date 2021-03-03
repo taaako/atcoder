a3ddd23dcb5e6bc20a368405d235f8a2c9601ab8
taaako <ta12ko.sa@gmail.com>
2021-03-03 19:16:06 +0900

    ACS_100の追加

---
+++ ABC/128_A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int apple, app;
+    cin >> apple >> app;
+
+    int applepie = 0;
+    applepie = (apple * 3 + app) / 2;
+
+    cout << applepie << endl;
+}
