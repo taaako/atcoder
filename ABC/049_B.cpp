3630aefd340e085fd83cbfc418bd7c75fc0c7334
taaako <ta12ko.sa@gmail.com>
2021-03-12 16:38:59 +0900

    ACS_200の追加

---
+++ ABC/049_B.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int h, w;
+    cin >> h >> w;
+    string mark;
+    for (int i = 0; i < h; i++) {
+        cin >> mark;
+        cout << mark << endl << mark << endl;
+    }
+}
