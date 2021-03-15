ab65a156cda8885f2995f842431a458fa12c9774
taaako <ta12ko.sa@gmail.com>
2021-03-15 11:07:14 +0900

    first contestの追加

---
+++ ABC/195_A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int mulhp, monhp;
+    cin >> mulhp >> monhp;
+
+    if (monhp % mulhp == 0)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+}
