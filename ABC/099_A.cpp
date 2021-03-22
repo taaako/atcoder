8496af086b73e1491cd1bf48211dbe52192e6c87
taaako <ta12ko.sa@gmail.com>
2021-03-22 23:46:12 +0900

    ACS_100の追加

---
+++ ABC/099_A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    if (n <= 999)
+        cout << "ABC" << endl;
+    else
+        cout << "ABD" << endl;
+}
