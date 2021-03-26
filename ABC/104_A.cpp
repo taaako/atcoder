0f519770f6eedcc06aae549d6333a48d2ef2aa7c
taaako <ta12ko.sa@gmail.com>
2021-03-26 23:39:56 +0900

    ACS_100の追加

---
+++ ABC/104_A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int r;
+    cin >> r;
+
+    if (r < 1200)
+        cout << "ABC" << endl;
+    else if (r < 2800)
+        cout << "ARC" << endl;
+    else
+        cout << "AGC" << endl;
+}
