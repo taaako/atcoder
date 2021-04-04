f136cd8b3b2114a75d55f73131a2433fcd696688
taaako <ta12ko.sa@gmail.com>
2021-04-04 22:56:40 +0900

    ACS_100の追加

---
+++ ABC/108_A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int k;
+    cin >> k;
+
+    int count = 0;
+    if (i % 2 == 0)
+        cout << pow(k / 2, 2) << endl;
+    else
+        cout << (k / 2) * (k / 2 + 1) << endl;
+}
