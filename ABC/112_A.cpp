18029720e00d1332b676d1273c39639dcd5f83e9
taaako <ta12ko.sa@gmail.com>
2021-04-09 02:46:20 +0900

    ACS_100の追加

---
+++ ABC/112_A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    if (n == 2) {
+        int a, b;
+        cin >> a >> b;
+        cout << a+b << endl;
+    }
+    else
+        cout << "Hello World" << endl;
+}
