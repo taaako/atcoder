c65702b3c1700176b75ea3d0c408ec2a1f80cf17
taaako <ta12ko.sa@gmail.com>
2021-04-10 00:33:56 +0900

    ACS_100の追加

---
+++ ABC/114_A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int age;
+    cin >> age;
+
+    if (age == 3 || age == 5 || age == 7)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
