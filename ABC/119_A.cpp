e69630c0b8abc177db7646501d528aed1919bc8a
taaako <ta12ko.sa@gmail.com>
2021-05-01 03:07:25 +0900

    add ACS_100

---
+++ ABC/119_A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+
+    if (s[5] == '0' && s[6] <= '4')
+        cout << "Heisei" << endl;
+    else
+        cout << "TBD" << endl;
+}
