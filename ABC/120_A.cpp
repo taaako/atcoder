72283e00461f9f7b6f931d6324630fd3f98576e4
taaako <ta12ko.sa@gmail.com>
2021-05-03 20:03:11 +0900

    add ACS_100

---
+++ ABC/120_A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (b / a >= c)
+        cout << c << endl;
+    else
+        cout << b/a << endl;
+}
