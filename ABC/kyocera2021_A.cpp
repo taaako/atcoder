0fab80631cdf3ebac5eb9ffbf6d6b0d810f6a8f5
taaako <ta12ko.sa@gmail.com>
2021-05-08 22:50:37 +0900

    add ABC_200

---
+++ ABC/kyocera2021_A.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int ans = (n - 1) / 100 + 1;
+    cout << ans << endl;
+}
---
+++ ABC/kyocera2021_B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long n, k;
+    cin >> n >> k;
+
+    for (int i = 0; i < k; i++) {
+        if (n % 200 == 0) {
+            n /= 200;
+        } else {
+            n = 1000 * n + 200;
+        }
+    }
+    cout << n << endl;
+}
